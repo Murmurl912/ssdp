92976fb6e2158dff77ba1643b8da3dfa30925252
murmur <murmurl912@outlook.com>
2021-06-04 20:43:20 +0800

    add manifest

--- src/main/java/org/example/ssdp/main/MainLauncher.java
+++ src/main/java/org/example/ssdp/main/MainLauncher.java
@@ -5,8 +5,19 @@ import org.example.ssdp.server.BroadcastServer;
 import java.io.IOException;
 
 public class MainLauncher {
+    private BroadcastServer server;
+
     public static void main(String[] args) throws IOException, InterruptedException {
         BroadcastServer server = new BroadcastServer();
         server.start();
+    }
+
+    private void command(String cmd) throws IOException, InterruptedException {
+        switch (cmd) {
+            case "START":
+                server.start();
+            case "STOP":
+                server.stop();
+        }
     }
 }
